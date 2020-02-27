5cdd45417fbfc26ad1dea5edcefbbfb285e5fa82
binding-archive-7591 <yarhumher@hotmail.com>
2020-02-27 23:03:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
+    counter = 1
+   names = dwarves.map do |a|
+   puts "#{counter}.#{a}"
+   counter += 1
+   end
+names
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
+  # Your code here
+  veggies.map do |capitalize|
+ capital_letter = capitalize[0].upcase
+letters = capitalize.split(//)
+letters.shift
+  "#{capital_letter}#{letters.join}!"
+ end
+end
+
+def long_planeteer_calls(calls_long)# code an argument here
+  calls_long.any? do |characters|
+    characters.size > 4
+  end
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(cheddar_cheese)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+     cheese_types = ["cheddar", "gouda", "camembert"]
+     cheddar_cheese.find do |cheese|
+     cheese.include?("cheddar")
+     end
 end
